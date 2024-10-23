45590bacf70ee444ae06e00229392e10ae9827dc
luchoaquinopla <luchoaquinopla@gmail.com>
2024-10-23 19:18:27 -0300

    logica del boton de google

--- src/auth/AuthHanddler.js
+++ src/auth/AuthHanddler.js
@@ -1,22 +1,32 @@
-
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth"; 
+import { auth } from './firebase.js';
 export const handleFacebookLogin = async () => {
-    const provider = new FacebookAuthProvider();
-    try {
-        const result = await signInWithPopup(auth, provider);
-        const user = result.user;
-        console.log("Usuario autenticado con Facebook:", user);
-    } catch (error) {
-        console.error("Error al iniciar sesión con Facebook:", error.message);
+ 
+};
+
+
+export const handleGoogleLogin = async() => {
+   const provider = new GoogleAuthProvider();
+    try{
+        const credentials = await signInWithPopup(auth,provider);
+        return{
+            username: credentials.user.displayName,
+            role: 'user',
+           
+            }
+        
+    }catch(error){
+        console.log("Error al iniciar sesión con Google:", error);
+        throw error;
+     
     }
-};
+}
 
+    
+ 
 
-export const handleGoogleLogin = () => {
-
-    window.location.href = 'https://accounts.google.com/';
-};
 
 export const handleInstagramLogin = () => {
 
-    window.location.href = 'https://www.instagram.com/';
+  
 };

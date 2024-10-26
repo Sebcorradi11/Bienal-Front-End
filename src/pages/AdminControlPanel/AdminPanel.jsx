5d5b9131b7f861bf675d9dbc3c6ad820ebe4ebe3
Sebcorradi11 <sebastianocorradi57@gmail.com>
2024-10-26 00:10:53 -0300

    Configuracion vite

--- src/pages/AdminControlPanel/AdminPanel.jsx
+++ src/pages/AdminControlPanel/AdminPanel.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Box, Grid, Paper, Typography } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
-import Header from '../../components/Header.jsx';
+import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
 const AdminControlPanel = () => {

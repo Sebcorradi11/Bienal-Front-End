222ef14f472ee46fc5f98fc6e98852acf9c0a345
Mario <133285366+DeuxExDoge@users.noreply.github.com>
2024-10-26 19:30:34 -0300

    Agregue el mocksSculptorVote.js y agregamos la iamgen y hicimos los cambios correspondientes a SculptorVote.jsx para su funcionamiento

---
+++ src/pages/Votos/mocksSculptorVote.js
@@ -0,0 +1,11 @@
+import BienalActual from '../../assets/vote/imagenPerfilEscultor.webp';
+
+
+export const SculptorImage = [
+    {
+        id: 1,
+        title: 'Frame del Bienal',
+        date: 'None',
+        image: BienalActual,
+    }
+];

81c6cf5cbf978c6ab8e3add23c297b3d996a3586
Sebcorradi11 <sebastianocorradi57@gmail.com>
2024-10-26 22:31:20 -0300

    Rutas privadas publicas pagina notfound

--- src/pages/GestionEventos/components/Escultores/VerEscultores.jsx
+++ src/pages/GestionEventos/components/Escultores/VerEscultores.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Typography, List, ListItem, ListItemText } from '@mui/material';
+import { Box, Typography, List, ListItem, ListItemText, Button } from '@mui/material';
 import HeaderPublic from '../../../../components/HeaderPublic';
 import Footer from '../../../../components/Footer';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';

0d73de27ee845135f06e35748ee6931aec8a3d16
felix toledo <felixtoledoctes@gmail.com>
2024-10-28 10:22:21 -0300

    rutas publicas cambiadas para que el admin tambien pueda acceder y usenavigate puesto en las formas

--- src/routes/PublicRoute.jsx
+++ src/routes/PublicRoute.jsx
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 
 const PublicRoute = ({ children }) => {
   const { isAuthenticated, role } = useSelector((state) => state.user);
-  return !isAuthenticated || role !== 'admin' ? children : <Navigate to="/adminPanel" />;
+  return children;
 };
 
 export default PublicRoute;

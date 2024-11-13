d868d0a2690bbeaf048f0da4abab9b72fd55a9bb
Felix Toledo <felixtoledoctes@gmail.com>
2024-11-13 20:33:45 -0300

    cambio de env

--- src/pages/Votos/VotesPage.jsx
+++ src/pages/Votos/VotesPage.jsx
@@ -6,12 +6,13 @@ import { useParams } from 'react-router-dom';
 
 const VotesPage = () => {
     const { id_escultor, id_evento, token } = useParams();
-    
+    //habilitado
+    const habilitado = true;
     console.log(id_escultor, id_evento, token)
     return (
         <>  
             <HeaderPublic />
-            <SculptorVote evento={id_evento} id_escultor={id_escultor}/>
+            {habilitado && <SculptorVote evento={id_evento} id_escultor={id_escultor}/>}
             <Footer />
         </>
     );
